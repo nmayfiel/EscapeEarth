f31ac10a96068dbf2ac4bbc1907cab8c2542c616
mastrella <michael_mastrella@hotmail.com>
2017-07-11 11:03:58 +0100

    adding changed input to my branch

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,65 @@
 #include "draw.hpp"
 #include <cstdio>
 
-int		key_press_hook(int keycode, GameData *Game)
+static void	change_key_state(t_key *key, bool is_down)
+{
+	if (key->ended_down != is_down)
+	{
+		key->changed = 1;
+		key->ended_down = is_down;
+	}
+	else
+		key->changed = 0;
+}
+
+int		key_down(int keycode, GameData *Game)
 {
 	if (keycode == K_EXIT){
 		exit(0);
 	}
-	if (keycode >= K_LEFT && keycode <= K_UP) {
-		Game->P1.Player_move(keycode);
-		Game->updated = 1;
+	if (keycode == K_UP || keycode == K_DOWN){
+			if (keycode == K_UP)
+					change_key_state(Game->input->k_up, 1);
+			else
+					change_key_state(Game->input->k_down, 1);
+	}
+	if (keycode == K_LEFT || keycode == K_RIGHT){
+			if (keycode == K_LEFT)
+					change_key_state(Game->input->k_left, 1);
+			else
+					change_key_state(Game->input->k_right, 1);
 	}
 	if (keycode == K_SPACE) {
-		//Game->P1.Player_shoot(keycode);
-		Game->updated = 1;
+		change_key_state(Game->input->k_space, 1);
+	}
+	if (keycode == K_ENTER) {
+		change_key_state(Game->input->k_enter, 1);
+	}
+	return (0);
+}
+
+int		key_down(int keycode, GameData *Game)
+{
+	if (keycode == K_EXIT){
+		exit(0);
+	}
+	if (keycode == K_UP || keycode == K_DOWN){
+			if (keycode == K_UP)
+					change_key_state(Game->input->k_up, 0);
+			else
+					change_key_state(Game->input->k_down, 0);
+	}
+	if (keycode == K_LEFT || keycode == K_RIGHT){
+			if (keycode == K_LEFT)
+					change_key_state(Game->input->k_left, 0);
+			else
+					change_key_state(Game->input->k_right, 0);
+	}
+	if (keycode == K_SPACE) {
+		change_key_state(Game->input->k_space, 0);
+	}
+	if (keycode == K_ENTER) {
+		change_key_state(Game->input->k_enter, 0);
 	}
 	return (0);
 }
@@ -30,11 +77,14 @@ int32_t		gameLoop(void *gameptr)
 
 //NOTE(Anthony): Upated goes 1 when any change occurs in GameData.
 // After displaying the new image, upadted goes to 0
-	if (game->updated == 1){
+
+	Player_move(game->input);
+
+	// if (game->updated == 1){
 		mlx_clear_window(game->mlx, game->win);
 		draw(game);
-	}
-	game->updated = 0;
+	// }
+	// game->updated = 0;
 
 // NOTE(nick): uncomment this to see that the game timer is working
 //	std::cout << game->currentTime() << std::endl;
@@ -50,8 +100,9 @@ int32_t		gameLoop(void *gameptr)
 int	main(void)
 {
 	GameData game = GameData("Escape EARTH", G_WIDTH, G_HEIGHT);
-	game.updated = 1;
-	game.SetKeyHook(&key_press_hook);
+	// game.updated = 1;
+	game.SetKeyHook(&key_down);
+	game.SetKeyHook(&key_up);
 	game.setLoopHook(&gameLoop);
 	game.Loop();
 	return (0);
